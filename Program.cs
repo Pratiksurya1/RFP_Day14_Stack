f2a9417f31622d8c1ba7b0ad5f85124df55411c2
Pratiksurya1 <suryawanshipratik356@gmail.com>
2022-01-21 21:46:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Test
+{
+    internal class Program
+    {
+        public static void Main(String[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Test
+{
+    internal class Stack
+    {
+        public Node head;
+        public void Push(int data)
+        {
+           
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head= node;
+            }
+            else
+            {
+                Node temp=this.head;
+                while(temp.next != null)
+                {
+                    temp=temp.next;
+                }
+                node.next = temp;
+                this.head = node;
+
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+    }
+}
